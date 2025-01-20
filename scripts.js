b731ab960e677ee99c48f3352ffb4d726313aa2c
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2025-01-20 06:59:02 +0000

    update

--- scripts.js
+++ scripts.js
@@ -25,6 +25,12 @@ function re() {
             case req0 == "https://postfolio.devlucas.page/api/about/":
                 document.getElementById('response0').innerHTML = "Hey, I'm Lucas! <br /> I'm high school student maker from São Paulo, Brazil! <br /> Here some things I like: < br /> <ul> <li> Aviation - Studying to be a private pilot :) </li> <li> 3d printers - Ender 3 Pro and Bambu Lab A1 mini owner! I provide maintenance and support </li> <li>CAD - Autodesk Fusion 360 and Onshape user, on-demand part modeling</li> </ul>";
                 break;
+            case req0 == "https://postfolio.devlucas.page/api/education":
+                document.getElementById('response0').innerHTML = "Escola Técnica Estadual de São Paulo - ETEC Zona Leste <br /> High School Diploma + Technical degree, Systems Devlopment <br /> Class of '26";
+                break;
+            case req0 == "https://postfolio.devlucas.page/api/languages":
+                document.getElementById('response0').innerHTML = "Portuguese (Brazil) <br /> English";
+                break;
             case req0 == "https://postfolio.devlucas.page/api/sp/":
                 window.location.href = "https://youtu.be/vuLtlzMMW6o"
                 document.getElementById('response0').innerHTML = "Enter the URL and click Send to get a response";
